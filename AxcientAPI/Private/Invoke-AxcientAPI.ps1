224d98aade4896da0403c4eaba0a187052ed50b1
adamburley <52085927+adamburley@users.noreply.github.com>
2024-06-30 14:12:33 -0400

    Updates

--- AxcientAPI/Private/Invoke-AxcientAPI.ps1
+++ AxcientAPI/Private/Invoke-AxcientAPI.ps1
@@ -9,17 +9,29 @@ function Invoke-AxcientAPI {
     $response = Invoke-WebRequest -Uri $_uri -Method $Method  -Headers @{ 'X-API-HEADER' = $AxcientApiKey } -SkipHttpErrorCheck
     Write-Debug -Message "API Returned: $($response.StatusCode) $($response.StatusDescription) $($response.Content.Length) bytes"
     if ($response.StatusCode -ne 200) {
-        Write-Error -Message "Failed to invoke Axcient API. StatusCode: $($response.StatusCode). Content: $($response.Content)"
+        Write-Debug -Message "Failed to invoke Axcient API. StatusCode: $($response.StatusCode). Content: $($response.Content)"
+        # The mock server is not fully featured for error codes, so some of this is guesswork.
         $parsedContent = if ($response.Content | Test-Json -ErrorAction SilentlyContinue) {
             $response.Content | ConvertFrom-Json
         }
         else {
             $response.Content
         }
+
+        $schemaError = switch ($response.StatusCode) { # This is from the OpenAPI schema
+            400 { 'One or more specified IDs are invalid' }
+            401 { 'Access token is missing or invalid' }
+            404 { 'Resource not found or delegated to authenticating user' }
+            default { "$($response.StatusCode) $($response.StatusDescription)" }
+        }
         
         [pscustomobject]@{
-            Response = $response.StatusCode
-            Content = $parsedContent
+            error = [pscustomobject][ordered]@{
+                Message = $schemaError
+                StatusCode = $response.StatusCode
+                StatusDescription = $response.StatusDescription
+                Content = $parsedContent
+            }
         }
     }
     else {
---
+++ AxcientAPI/Public/Get-BackupJobHistory.ps1
@@ -0,0 +1,45 @@
+function Get-BackupJobHistory {
+    [CmdletBinding(DefaultParameterSetName = 'Device')]
+    param (        
+        [ValidateScript({ Find-ObjectIdByReference -Reference $_ -Schema 'device' -Validation }, ErrorMessage = 'Must be a positive integer or matching object' )]
+        [object]$Device,
+
+        [Parameter()]
+        [ValidateScript({ Find-ObjectIdByReference -Reference $_ -Schema 'client' -Validation }, ErrorMessage = 'Must be a positive integer or matching object' )]
+        [object]$Client,
+
+        [Parameter(ValueFromPipeline,Mandatory)]
+        [ValidateScript({ Find-ObjectIdByReference -Reference $_ -Schema 'job' -Validation }, ErrorMessage = 'Must be a positive integer or matching object' )]
+        [object]$Job
+    )
+    begin {
+        $deviceParamID = Find-ObjectIdByReference $Device
+        $clientParamID = Find-ObjectIdByReference $Client
+    }
+    process {
+        $_jobId = Find-ObjectIdByReference $Job
+        $_clientId = $Job.client_id ?? $clientParamID
+        $_deviceId = $Job.device_id ?? $deviceParamID
+        Write-Debug "Get-BackupJobHistory: Client: $_clientId, Device: $_deviceId, Job: $_jobId"
+        if ($null -eq $_jobId -or $null -eq $_clientId -or $null -eq $_deviceId) {
+            Write-Error "Missing client ID, device ID, or job ID. All three are required and may be included in the Job object or specified as parameters."
+            continue
+        }
+        $_endpoint = "client/$_clientId/device/$_deviceId/job/$_jobId/history"
+        $call = Invoke-AxcientAPI -Endpoint $_endpoint -Method Get
+        if ($call.error) {
+            $_errorMessage = $call.error.Message
+            Write-Error -Message "Get-Client returned $_errorMessage"
+            $call
+        }
+        else {
+            $call | Foreach-Object { 
+                $_ | Add-Member -MemberType NoteProperty -Name 'client_id' -Value $_clientId -PassThru | 
+                Add-Member -MemberType NoteProperty -Name 'device_id' -Value $_deviceId -PassThru |
+                Add-Member -MemberType NoteProperty -Name 'job_id' -Value $_jobId -PassThru |
+                Add-Member -MemberType NoteProperty -Name 'objectschema' -Value 'job.history' -PassThru
+            }
+        }
+
+    }
+}
