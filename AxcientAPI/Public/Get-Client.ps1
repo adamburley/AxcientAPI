62564e5c5089243bdc69f671feba423f1d11c0f4
adamburley <52085927+adamburley@users.noreply.github.com>
2024-06-30 15:33:23 -0400

    Update to match parameters and processing

--- AxcientAPI/Public/Get-Client.ps1
+++ AxcientAPI/Public/Get-Client.ps1
@@ -1,19 +1,30 @@
-function Get-Client {
+﻿function Get-Client {
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [Alias('Id_')]
-        [int]$Id,
-        
+        [Parameter(ValueFromPipeline, Mandatory)]
+        [Alias('Id')]
+        [ValidateScript({ Find-ObjectIdByReference -Reference $_ -Schema 'client' -Validation }, ErrorMessage = 'Must be a positive integer or matching object' )]
+        [object[]]$Client,
+
+        [Parameter()]
         [switch]$IncludeAppliances
     )
-    $call = Invoke-AxcientAPI -Endpoint "client/$Id" -Method Get
-    if ($call.error){
-        $_errorMessage = $call.error.Message
-        Write-Error -Message "Get-Client returned $_errorMessage"
-        $call
-    }
-    else {
-        $call | Foreach-Object { $_ | Add-Member -MemberType NoteProperty -Name 'objectschema' -Value 'client' -PassThru }
+
+    process {
+        foreach ($thisClient in $Client) {
+            $_endpoint = "client/$Id"
+            if ($IncludeAppliances) {
+                $_endpoint += '?include_appliances=true'
+            }
+            $call = Invoke-AxcientAPI -Endpoint $_endpoint -Method Get
+            if ($call.error) {
+                $_errorMessage = $call.error.Message
+                Write-Error -Message "Get-Client returned $_errorMessage"
+                $call
+            }
+            else {
+                $call | Foreach-Object { $_ | Add-Member -MemberType NoteProperty -Name 'objectschema' -Value 'client' -PassThru }
+            }
+        }
     }
 }
